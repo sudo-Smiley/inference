4e214f6e7915ebe79888fb5830b4a77b0ca79064
Omar Naman <onaman@uwaterloo.ca>
2021-06-01 03:13:20 -0400

    introduce runner without loadgen for testing models

---
+++ vision/classification_and_detection/python/no_loadgen.py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python3
+
+import dataset
+import argparse
+import coco
+import imagenet
+import os
+import argparse
+import time
+import cli_colors
+
+SUPPORTED_DATASETS = {
+    "imagenet":
+        (imagenet.Imagenet, dataset.pre_process_vgg, dataset.PostProcessCommon(offset=-1),
+         {"image_size": [224, 224, 3]}),
+    "imagenet_mobilenet":
+        (imagenet.Imagenet, dataset.pre_process_mobilenet, dataset.PostProcessArgMax(offset=-1),
+         {"image_size": [224, 224, 3]}),
+    "imagenet_pytorch":
+        (imagenet.Imagenet, dataset.pre_process_imagenet_pytorch, dataset.PostProcessArgMax(offset=0),
+         {"image_size": [224, 224, 3]}),
+    "coco-300":
+        (coco.Coco, dataset.pre_process_coco_mobilenet, coco.PostProcessCoco(),
+         {"image_size": [300, 300, 3]}),
+    "coco-300-pt":
+        (coco.Coco, dataset.pre_process_coco_pt_mobilenet, coco.PostProcessCocoPt(False, 0.3),
+         {"image_size": [300, 300, 3]}),
+    "coco-1200":
+        (coco.Coco, dataset.pre_process_coco_resnet34, coco.PostProcessCoco(),
+         {"image_size": [1200, 1200, 3]}),
+    "coco-1200-onnx":
+        (coco.Coco, dataset.pre_process_coco_resnet34, coco.PostProcessCocoOnnx(),
+         {"image_size": [1200, 1200, 3]}),
+    "coco-1200-pt":
+        (coco.Coco, dataset.pre_process_coco_resnet34, coco.PostProcessCocoPt(True, 0.05),
+         {"image_size": [1200, 1200, 3], "use_label_map": True}),
+    "coco-1200-tf":
+        (coco.Coco, dataset.pre_process_coco_resnet34, coco.PostProcessCocoTf(),
+         {"image_size": [1200, 1200, 3], "use_label_map": False}),
+}
+
+SUPPORTED_PROFILES = {
+    "defaults": {
+        "dataset": "imagenet",
+        "backend": "tensorflow",
+        "cache": 0,
+        "max-batchsize": 32,
+    },
+
+    # resnet
+    "resnet50-tf": {
+        "inputs": "input_tensor:0",
+        "outputs": "ArgMax:0",
+        "dataset": "imagenet",
+        "backend": "tensorflow",
+        "model-name": "resnet50",
+    },
+    "resnet50-onnxruntime": {
+        "dataset": "imagenet",
+        "outputs": "ArgMax:0",
+        "backend": "onnxruntime",
+        "model-name": "resnet50",
+    },
+
+    # mobilenet
+    "mobilenet-tf": {
+        "inputs": "input:0",
+        "outputs": "MobilenetV1/Predictions/Reshape_1:0",
+        "dataset": "imagenet_mobilenet",
+        "backend": "tensorflow",
+        "model-name": "mobilenet",
+    },
+    "mobilenet-onnxruntime": {
+        "dataset": "imagenet_mobilenet",
+        "outputs": "MobilenetV1/Predictions/Reshape_1:0",
+        "backend": "onnxruntime",
+        "model-name": "mobilenet",
+    },
+
+    # ssd-mobilenet
+    "ssd-mobilenet-tf": {
+        "inputs": "image_tensor:0",
+        "outputs": "num_detections:0,detection_boxes:0,detection_scores:0,detection_classes:0",
+        "dataset": "coco-300",
+        "backend": "tensorflow",
+        "model-name": "ssd-mobilenet",
+    },
+    "ssd-mobilenet-pytorch": {
+        "inputs": "image",
+        "outputs": "bboxes,labels,scores",
+        "dataset": "coco-300-pt",
+        "backend": "pytorch-native",
+        "model-name": "ssd-mobilenet",
+    },
+    "ssd-mobilenet-onnxruntime": {
+        "dataset": "coco-300",
+        "outputs": "num_detections:0,detection_boxes:0,detection_scores:0,detection_classes:0",
+        "backend": "onnxruntime",
+        "data-format": "NHWC",
+        "model-name": "ssd-mobilenet",
+    },
+
+    # ssd-resnet34
+    "ssd-resnet34-tf": {
+        "inputs": "image:0",
+        "outputs": "detection_bboxes:0,detection_classes:0,detection_scores:0",
+        "dataset": "coco-1200-tf",
+        "backend": "tensorflow",
+        "data-format": "NCHW",
+        "model-name": "ssd-resnet34",
+    },
+    "ssd-resnet34-pytorch": {
+        "inputs": "image",
+        "outputs": "bboxes,labels,scores",
+        "dataset": "coco-1200-pt",
+        "backend": "pytorch-native",
+        "model-name": "ssd-resnet34",
+    },
+    "ssd-resnet34-onnxruntime": {
+        "dataset": "coco-1200-onnx",
+        "inputs": "image",
+        "outputs": "bboxes,labels,scores",
+        "backend": "onnxruntime",
+        "data-format": "NCHW",
+        "max-batchsize": 1,
+        "model-name": "ssd-resnet34",
+    },
+    "ssd-resnet34-onnxruntime-tf": {
+        "dataset": "coco-1200-tf",
+        "inputs": "image:0",
+        "outputs": "detection_bboxes:0,detection_classes:0,detection_scores:0",
+        "backend": "onnxruntime",
+        "data-format": "NHWC",
+        "model-name": "ssd-resnet34",
+    },
+}
+
+
+def get_backend(backend):
+    if backend == "tensorflow":
+        from backend_tf import BackendTensorflow
+        backend = BackendTensorflow()
+    elif backend == "onnxruntime":
+        from backend_onnxruntime import BackendOnnxruntime
+        backend = BackendOnnxruntime()
+    elif backend == "null":
+        from backend_null import BackendNull
+        backend = BackendNull()
+    elif backend == "pytorch":
+        from backend_pytorch import BackendPytorch
+        backend = BackendPytorch()
+    elif backend == "pytorch-native":
+        from backend_pytorch_native import BackendPytorchNative
+        backend = BackendPytorchNative()
+    elif backend == "tflite":
+        from backend_tflite import BackendTflite
+        backend = BackendTflite()
+    else:
+        raise ValueError("unknown backend: " + backend)
+    return backend
+
+
+def get_args():
+    """Parse commandline."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset", choices=SUPPORTED_DATASETS.keys(), help="dataset")
+    parser.add_argument("--dataset-path", required=True,
+                        help="path to the dataset")
+    parser.add_argument("--dataset-list", help="path to the dataset list")
+    parser.add_argument(
+        "--data-format", choices=["NCHW", "NHWC"], help="data format")
+    parser.add_argument(
+        "--profile", choices=SUPPORTED_PROFILES.keys(), help="standard profiles")
+    parser.add_argument("--scenario", default="SingleStream",
+                        )
+    parser.add_argument("--max-batchsize", type=int,
+                        help="max batch size in a single inference")
+    parser.add_argument("--model", required=True, help="model file")
+    parser.add_argument("--output", default="output", help="test results")
+    parser.add_argument("--inputs", help="model inputs")
+    parser.add_argument("--outputs", help="model outputs")
+    parser.add_argument("--backend", help="runtime to use")
+    parser.add_argument(
+        "--model-name", help="name of the mlperf model, ie. resnet50")
+    parser.add_argument("--threads", default=os.cpu_count(),
+                        type=int, help="threads")
+    parser.add_argument("--qps", type=int, help="target qps")
+    parser.add_argument("--cache", type=int, default=0, help="use cache")
+    parser.add_argument("--accuracy", action="store_true",
+                        help="enable accuracy pass")
+    parser.add_argument("--find-peak-performance", action="store_true",
+                        help="enable finding peak performance pass")
+    parser.add_argument("--debug", action="store_true",
+                        help="debug, turn traces on")
+
+    # file to use mlperf rules compliant parameters
+    parser.add_argument(
+        "--mlperf_conf", default="../../mlperf.conf", help="mlperf rules config")
+    # file for user LoadGen settings such as target QPS
+    parser.add_argument("--user_conf", default="user.conf",
+                        help="user config for user LoadGen settings such as target QPS")
+
+    # below will override mlperf rules compliant settings - don't use for official submission
+    parser.add_argument("--time", type=int, help="time to scan in seconds")
+    parser.add_argument("--count", type=int, help="dataset items to use")
+    parser.add_argument("--max-latency", type=float,
+                        help="mlperf max latency in pct tile")
+    parser.add_argument("--samples-per-query", type=int,
+                        help="mlperf multi-stream sample per query")
+    parser.add_argument("--model-threads", type=int, default=0,
+                        help="the number of threads the model should run for inferencing a single query")
+    args = parser.parse_args()
+
+    # don't use defaults in argparser. Instead we default to a dict, override that with a profile
+    # and take this as default unless command line give
+    defaults = SUPPORTED_PROFILES["defaults"]
+
+    if args.profile:
+        profile = SUPPORTED_PROFILES[args.profile]
+        defaults.update(profile)
+    for k, v in defaults.items():
+        kc = k.replace("-", "_")
+        if getattr(args, kc) is None:
+            setattr(args, kc, v)
+    if args.inputs:
+        args.inputs = args.inputs.split(",")
+    if args.outputs:
+        args.outputs = args.outputs.split(",")
+
+    return args
+
+
+def main():
+    args = get_args()
+    backend = get_backend("onnxruntime")
+    image_format = args.data_format if args.data_format else backend.image_format()
+    wanted_dataset, pre_proc, post_proc, kwargs = SUPPORTED_DATASETS[args.dataset]
+    cli_colors.color_print("Building Dataset", cli_colors.YELLOW, cli_colors.MOD_BOLD)
+    ds = wanted_dataset(data_path=args.dataset_path,
+                        image_list=args.dataset_list,
+                        name=args.dataset,
+                        image_format=image_format,
+                        pre_process=pre_proc,
+                        use_cache=args.cache,
+                        count = args.count,
+                        **kwargs)
+    model = backend.load(args.model, inputs=args.inputs, outputs=args.outputs, threads=args.model_threads)
+    final_results = {
+        "runtime": model.name(),
+        "version": model.version(),
+        "time": int(time.time()),
+        "cmdline": str(args),
+    }
+    count = ds.get_item_count()
+    query_size = args.samples_per_query
+    cli_colors.color_print(f"Total Samples: {count}, Query Size: {query_size}", cli_colors.YELLOW_SHADE2)
+    
+    ds.load_query_samples(list(range(count)))
+    for i in range(0, count, query_size):
+        img, _ = ds.get_samples(list(range(i, i+query_size)))
+        _ = backend.predict({backend.inputs[0]: img})
+    ds.unload_query_samples(None)
+    
+if __name__ == "__main__":
+    main()
